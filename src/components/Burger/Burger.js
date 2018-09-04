9c1fa567b82818645b636ed463efc4e833368c91
Ferdroid8080 <andresf@ferudinato.com>
2018-09-04 07:36:19 -0500

    agregado los ingredientes al componente Burger de manera dinamica a través de estados

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,16 @@ import Stylesheet from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => 
+                <BurgerIngredient key={igKey + i} type={igKey} />)
+        });
+
     return (
         <div className={Stylesheet.Burger}>
         <BurgerIngredient type="bread-top" />
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
-        <BurgerIngredient type="salad" />
+        {transformedIngredients}
         <BurgerIngredient type="bread-bottom" />
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,25 @@ import Burger from '../../components/Burger/Burger';
 
 
 class BurgerBuilder extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            ingredients: {
+                salad: 1,
+                bacon: 1,
+                cheese: 3,
+                meat: 2
+            }
+        }
+
+    }
+
+
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build controls</div>
             </Aux>
         )
