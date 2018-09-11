e1db993c508dc3f4ec2af5b7164a210e65fe052c
Ferdroid8080 <andresf@ferudinato.com>
2018-09-11 18:15:24 -0500

    corregido el atributo disabled cuando no hay ninguna cantidad del ingrediente seleccionado

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,24 +4,15 @@ import Stylesheet from './BuildControls.css';
 
 import BuildControl from './BuildControl/BuildControl';
 
-// const controls = [
-//     { label: 'Cebolla', type: 'salad' },
-//     { label: 'Tocino', type: 'bacon' },
-//     { label: 'Carne', type: 'meat' },
-//     { label: 'Queso', type: 'cheese' },
-// ]
-
 
 const buildControls = (props) => {
-    const disabledIngredientCount = Object.keys(props.disabled).length
-
     return (
         <div className={Stylesheet.BuildControls}>
             <p>Total a pagar: <strong>${props.price.toFixed(2)} USD</strong></p>
             {props.controls.map((ctrl, index) => (
                 <BuildControl key={ctrl.id} label={ctrl.label} 
                     added={() => props.onAddedIngredient(index, ctrl.type)} removed={() => props.onRemovedIngredient(index, ctrl.type)}
-                    disabled={disabledIngredientCount > 0 ? props.disabled[ctrl.type] : true} />
+                    disabled={props.disabled[ctrl.type] !== undefined ? props.disabled[ctrl.type] : true} />
             ))}
             <button className={Stylesheet.OrderButton} 
                 disabled={!props.purchasable}
