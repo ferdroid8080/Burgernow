6d8df8d48c8c72379b56063e973898e702a209b9
Ferdroid8080 <andresf@ferudinato.com>
2018-10-01 16:06:49 -0500

    agregado un spinner en la ui del resumen de pedido para mostrar mientras se ejecuta la peticion http

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
+import Spinner from '../../UI/Spinner/Spinner';
 
 import Stylesheet from './OrderSummary.css';
 
@@ -26,27 +27,31 @@ const orderSummary = (props) => {
         })
 
     return (
-        <Aux>
-            <h2>Detalles de su orden</h2>
-            <p>Su deliciosa hamburguesa con los siguientes sabores:</p>
-            <table className={Stylesheet.CustomTable}>
-                <thead>
-                    <tr>
-                        <td>Ingrediente</td>
-                        <td>Cantidad</td>
-                        <td>Precio Unitario (<strong>USD</strong>)</td>
-                        <td>Precio Total</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {ingredientSummary}
-                </tbody>
-            </table>
-            <p><strong>Valor a pagar: ${props.price.toFixed(2)} USD</strong></p>
-            <p>¿Esta bien que continue con la compra?</p>
-            <Button btnType="Danger" btnClicked={props.purchaseCancelledHandler}>cancelar</Button>
-            <Button btnType="Success" btnClicked={props.purchaseContinuedHandler}>pagar</Button>
-        </Aux>
+        props.loading ? 
+            <Spinner /> : 
+            (
+                <Aux>
+                    <h2>Detalles de su orden</h2>
+                    <p>Su deliciosa hamburguesa con los siguientes sabores:</p>
+                    <table className={Stylesheet.CustomTable}>
+                        <thead>
+                            <tr>
+                                <td>Ingrediente</td>
+                                <td>Cantidad</td>
+                                <td>Precio Unitario (<strong>USD</strong>)</td>
+                                <td>Precio Total</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {ingredientSummary}
+                        </tbody>
+                    </table>
+                    <p><strong>Valor a pagar: ${props.price.toFixed(2)} USD</strong></p>
+                    <p>¿Esta bien que continue con la compra?</p>
+                    <Button btnType="Danger" btnClicked={props.purchaseCancelledHandler}>cancelar</Button>
+                    <Button btnType="Success" btnClicked={props.purchaseContinuedHandler}>pagar</Button>
+                </Aux>
+            ) 
     )
 }
 
---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Stylesheet from './Spinner.css';
+
+const spinner = () => (
+    <div className={Stylesheet.Loader}>Espere un momento...</div>
+)
+
+export default spinner;
