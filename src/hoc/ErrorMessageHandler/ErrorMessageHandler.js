08ac2b26306ae82a91d32569fe330922b55efcbd
Ferdroid8080 <andresf@ferudinato.com>
2018-10-02 08:32:41 -0500

    se eliminan los interceptors del error handler para evitar leaks de memoria cuando se instancie en multiples componentes

--- src/hoc/ErrorMessageHandler/ErrorMessageHandler.js
+++ src/hoc/ErrorMessageHandler/ErrorMessageHandler.js
@@ -12,14 +12,20 @@ const errorMessage = (WrappedComponent, axios) => {
 
         componentWillMount() {
             console.log('[WillMount] errorMessage')
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null})
                 return req
             })
 
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor =  axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error})
             })
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor)
+            axios.interceptors.response.eject(this.resInterceptor)
+            console.log('[WillUnmount]', this.reqInterceptor, this.resInterceptor)
         }
 
         render() {
