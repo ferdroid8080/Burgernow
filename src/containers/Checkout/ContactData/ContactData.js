3b423bb9d86386e93bcffa32f2dd821f955ec50d
Ferdroid8080 <andresf@ferudinato.com>
2018-10-04 16:25:27 -0500

    se reciben los datos de la compra por props en contactdata y se envia la peticion al backend

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import axios from '../../../axios';
 
 import Stylesheet from './ContactData.css';
 
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
     state = {
@@ -12,21 +14,62 @@ class ContactData extends Component {
             street: '',
             postalCode: '',
             phone: ''
+        },
+        loadingPurchase: false,
+        sentPurchase: false
+    }
+
+    orderClickHandler = (ev) => {
+        ev.preventDefault()
+
+        const pedidoData = {
+            ingredientes: this.props.ingredients,
+            precio: this.props.price,
+            cliente: {
+                nombre: this.state.name,
+                direccion: this.state.address.street,
+                postalcode: this.state.postalCode,
+                telefono: this.state.address.phone,
+                email: this.state.address.phone
+            },
+            metodoEntrega: 'rapida',
         }
+        this.setState({loadingPurchase: true})
+        
+        axios.post('/pedidos.json', pedidoData)
+            .then(response => {
+                this.setState({loadingPurchase: false, sentPurchase: true})
+            })
+            .catch(error => {
+                this.setState({loadingPurchase: false})
+            })
     }
 
     render() {
+        let form = null
+        if (this.state.loadingPurchase) {
+            form = <Spinner />
+        } else {
+            if (this.state.sentPurchase) {
+                form = <p style={{textAlign: 'center'}}>Order was sent!</p>
+            } else {
+                form = (
+                    <form>
+                        <legend>Enter your contact details</legend>
+                        <input className={Stylesheet.Input} type='text' name='name' value={this.state.name} onChange={(e) => this.setState({name: e.target.value})} placeholder='Your name' />
+                        <input className={Stylesheet.Input} type='email' name='email' value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} placeholder='Your email' />
+                        <input className={Stylesheet.Input} type='text' name='street' value={this.state.address.street} placeholder='Your street address' />
+                        <input className={Stylesheet.Input} type='text' name='postal' value={this.state.address.postalCode} placeholder='Postal Code' />
+                        <input className={Stylesheet.Input} type='text' name='phone' value={this.state.address.phone} placeholder='Your phone number' />
+                        <Button btnType='Success' btnClicked={this.orderClickHandler}>ORDER</Button>
+                    </form>
+                )
+            }
+            
+        }
         return (
             <div className={Stylesheet.ContactData}>
-                <form>
-                    <legend>Enter your contact details</legend>
-                    <input className={Stylesheet.Input} type='text' name='name' placeholder='Your name' />
-                    <input className={Stylesheet.Input} type='email' name='email' placeholder='Your email' />
-                    <input className={Stylesheet.Input} type='text' name='street' placeholder='Your street address' />
-                    <input className={Stylesheet.Input} type='text' name='postal' placeholder='Postal Code' />
-                    <input className={Stylesheet.Input} type='text' name='phone' placeholder='Your phone number' />
-                    <Button btnType='Success'>ORDER</Button>
-                </form>
+                {form}
             </div>
         )
     }
