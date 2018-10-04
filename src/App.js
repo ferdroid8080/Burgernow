799b5c7ef58b68723ca3221f027f9659c49ec519
Ferdroid8080 <andresf@ferudinato.com>
2018-10-04 15:14:17 -0500

    se agrego la dependencia react-router y se modifico el App para permitir rutas al builder y al checkout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
 import './App.css';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />          
+            <Switch>
+              <Route exact path='/builder' component={BurgerBuilder} />
+              <Route exact path='/checkout' component={Checkout} />
+              <Redirect exact from='/' to='/builder' />
+              <Route render={() => <h3 style={{textAlign: 'center'}}>404 Page not found</h3>} />
+            </Switch>
         </Layout>
       </div>
     );
