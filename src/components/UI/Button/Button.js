8653a9acc27f75c0da42613b6aa6d8eae71208b2
Ferdroid8080 <andresf@ferudinato.com>
2018-10-08 18:17:52 -0500

    se agrego un prop al boton que permite deshabilitarlo y se agregaron sus respectivos estilos css cuando esta desactivado

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,7 +3,9 @@ import React from 'react';
 import Stylesheet from './Button.css';
 
 const button = (props) => (
-    <button className={[Stylesheet.Button, Stylesheet[props.btnType]].join(' ')}
+    <button 
+        disabled={props.disabled}
+        className={[Stylesheet.Button, Stylesheet[props.btnType]].join(' ')}
         onClick={props.btnClicked}>{props.children}</button>
 );
 
