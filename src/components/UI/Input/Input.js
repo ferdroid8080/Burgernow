ecf1fe76fee0462d412d97e4994a1329f62a7b0f
Ferdroid8080 <andresf@ferudinato.com>
2018-10-08 18:16:54 -0500

    se agrego un nuevo props al Input que permite mostrar un mensaje de validacion

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,19 +3,26 @@ import React from 'react';
 import Stylesheet from './Input.css';
 
 const inputCustom = (props) => {
-    let inputElement = null
+    let inputElement = null, validationMessageError = null
+    let inputClass = [Stylesheet.InputElement]
+
+    if (!props.valid && props.shouldValidate && props.touched) {
+        inputClass.push(Stylesheet.Invalid)
+        validationMessageError = <p style={{margin: 0}}>Por favor ingrese informacion valida</p>
+    }
+
     if (props.elementType === 'input') {
         inputElement = <input 
-                            className={Stylesheet.InputElement} 
+                            className={inputClass.join(' ')} 
                             {...props.elementConfig} 
                             value={props.value} onChange={props.changed} />
     } else if (props.elementType === 'textarea') {
         inputElement = <textarea 
-                            className={Stylesheet.InputElement} 
+                            className={inputClass.join(' ')} 
                             {...props.elementConfig} onChange={props.changed}>{props.children}</textarea>
     } else if (props.elementType === 'select') {
         inputElement = (
-            <select value={props.value} className={Stylesheet.SelectInput} onChange={props.changed}>
+            <select value={props.value} className={inputClass.join(' ')} onChange={props.changed}>
                 {props.elementConfig.options.map(opt =>
                     <option key={opt.value} value={opt.value}>{opt.displayValue}</option>
                 )}
@@ -23,7 +30,7 @@ const inputCustom = (props) => {
         )
     } else {
         inputElement = <input 
-                            className={Stylesheet.InputElement} 
+                            className={inputClass.join(' ')} 
                             {...props.elementConfig} onChange={props.changed} />
     }
 
@@ -31,6 +38,7 @@ const inputCustom = (props) => {
         <div className={Stylesheet.Input}>
             <label className={Stylesheet.Label}>{props.label}</label>
             {inputElement}
+            {validationMessageError}
         </div>
     )
 }
