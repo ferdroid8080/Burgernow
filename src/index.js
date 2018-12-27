50c65d50e451165013058c7f13445e21829bd7ab
Ferdroid8080 <andresf@ferudinato.com>
2018-12-27 18:34:02 -0500

    se invoca el reducer y el provider desde el index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
+
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 registerServiceWorker();
