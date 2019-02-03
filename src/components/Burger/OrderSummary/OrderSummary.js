b5bd54dc4391ecec93117037985c3ca57bad2e24
Ferdroid8080 <andresf@ferudinato.com>
2019-02-03 11:35:55 -0500

    arreglado la tabulacion de los ingredientes en la ventana del pedido

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -10,21 +10,21 @@ import Stylesheet from './OrderSummary.css';
 const orderSummary = (props) => {
     let ingredientSummary = null
     if (props.ingredients) {
-        ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            const itemDetail = props.ingredients[igKey]
-            return (
-                props.ingredients[igKey]
-                ?
-                    <tr key={igKey}>
-                        <td>{itemDetail.label}</td>
-                        <td className='text-center'>{''}</td>
-                        <td>$ {itemDetail.price.toLocaleString()}</td>
-                        <td>$ {''}</td>
-                    </tr>
-                : null
-            )
-        })
+        ingredientSummary = props.ingredients
+            .map((i, index) => {
+                let item = null
+                if (i.count && i.count > 0) {
+                    item = (
+                        <tr key={i.id + '_' + index}>
+                            <td>{i.label}</td>
+                            <td className='text-center'>{i.count}</td>
+                            <td>$ {i.price.toLocaleString()}</td>
+                            <td>$ {(i.price * i.count).toLocaleString()}</td>
+                        </tr>
+                    )
+                }
+                return item
+            })
     }
 
     return (
@@ -47,7 +47,7 @@ const orderSummary = (props) => {
                             {ingredientSummary}
                         </tbody>
                     </table>
-                    <p><strong>Valor a pagar: ${props.price.toFixed(2)} USD</strong></p>
+                    <p><strong>Valor a pagar: ${props.price.toLocaleString()} COP</strong></p>
                     <p>¿Esta bien que continue con la compra?</p>
                     <Button btnType="Danger" btnClicked={props.purchaseCancelledHandler}>cancelar</Button>
                     <Button btnType="Success" btnClicked={props.purchaseContinuedHandler}>pagar</Button>
