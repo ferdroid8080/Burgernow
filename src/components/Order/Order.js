6994c2861057e74a253bd3a5e78de95de3094fef
Ferdroid8080 <andresf@ferudinato.com>
2019-02-04 11:28:05 -0500

    corregido el filtrado del arreglo de ordenes

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,27 +3,23 @@ import React from 'react';
 import Stylesheet from './Order.css';
 
 const order = (props) => {
-    const ingredientsList = []
-
-    for (let name in props.ingredients) {
-        ingredientsList.push({name: name, amount: props.ingredients[name]})
-    }
+    const ingredientsList = props.ingredients.filter(v => v.count && v.count > 0)
     
     const ingredientOutput = ingredientsList.map(ig => 
         <span 
-            key={ig.name} 
+            key={ig.id} 
             style={{
                 textTransform: 'capitalize',
                 margin: '0px 4px',
                 padding: '4px',
                 border: '1px solid #ccc'
-            }}>{ig.name} ({ig.amount})</span>
+            }}>{ig.label} ({ig.count})</span>
     )
 
     return (
         <div className={Stylesheet.Order}>
-            <p>Ingredients: {ingredientOutput}</p>
-            <p>Price: <strong>COP ${props.price.toFixed(0)}</strong></p>
+            <p>Ingredientes: {ingredientOutput}</p>
+            <p>Total a pagar: <strong>COP ${props.price.toLocaleString()}</strong></p>
         </div>
     )
 }
