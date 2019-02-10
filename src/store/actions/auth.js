2c8708cb7c11db312f29bdafbf47ae68ac7d9f20
Ferdroid8080 <andresf@ferudinato.com>
2019-02-10 10:40:04 -0500

    se agrego la autenticacion persistente con localStorage y reforzaron los props con withRouter

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,9 @@ const authFail = (error) => {
 }
 
 export const logout = () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('userId')
+    localStorage.removeItem('expirationDate')
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -49,6 +52,10 @@ export const auth = (email, passwd, isSignup) => {
             returnSecureToken: true
         }).then(response => {
                 console.log(response)
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000)
+                localStorage.setItem('token', response.data.idToken)
+                localStorage.setItem('userId', response.data.localId)
+                localStorage.setItem('expirationDate', expirationDate)
                 dispatch(authSuccess(response.data))
                 dispatch(checkAuthExpirationTime(response.data.expiresIn))
             })
@@ -63,5 +70,24 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    }
+}
+
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token')
+        if (!token) {
+            dispatch(logout())
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'))
+            const userId = localStorage.getItem('userId')
+            if (expirationDate <= new Date()) {
+                dispatch(logout())
+            } else {
+                dispatch(authSuccess({localId: userId, idToken: token}))
+                dispatch(checkAuthExpirationTime((expirationDate.getTime() - new Date().getTime()) / 1000))
+            }
+        }
     }
 }
