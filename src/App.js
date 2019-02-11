572427baa596f5b9577999d4abd997b411e7faaf
Ferdroid8080 <andresf@ferudinato.com>
2019-02-11 12:08:53 -0500

    implementado el lazy loading para rutas que no se ven todo el tiempo

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import { connect } from 'react-redux';
 
 import './App.css';
 
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout')
+})
+
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth')
+})
+
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders')
+})
 
 
 class App extends Component {
@@ -21,7 +32,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path='/auth' component={Auth} />
+        <Route path='/auth' component={asyncAuth} />
         <Route path='/' component={BurgerBuilder} />
         <Redirect to='/' />
       </Switch>
@@ -30,10 +41,10 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
+          <Route path='/checkout' component={asyncCheckout} />
+          <Route path='/orders' component={asyncOrders} />
           <Route path='/logout' component={Logout} />
-          <Route path='/auth' component={Auth} /> {/* se debe activar esta ruta aqui para que pueda redireccionar al checkout correctamente */}
+          <Route path='/auth' component={asyncAuth} /> {/* se debe activar esta ruta aqui para que pueda redireccionar al checkout correctamente */}
           <Route path='/' component={BurgerBuilder} />
           <Redirect to='/' />
         </Switch>
---
+++ src/hoc/asyncComponent/asyncComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+const asyncComponent = (importComponent) => {
+    return class extends Component {
+        state = {
+            component: null
+        }
+
+        componentDidMount() {
+            importComponent()
+                .then(cmp => {
+                    this.setState({component: cmp.default})
+                })
+        }
+
+        render() {
+            const Cmp = this.state.component
+            return Cmp ? <Cmp {...this.props} /> : null
+        }
+    }
+}
+
+export default asyncComponent;
