bdd93dcb0a273466d6aef9acc3d299d197f2e301
Ferdroid8080 <andresf@ferudinato.com>
2019-02-11 11:44:20 -0500

    se oculta redux en entorno de produccion

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import ordersReducer from './store/reducers/orders';
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
