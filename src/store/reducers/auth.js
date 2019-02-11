99ec7fd2cf70da27ad83de2c232120ef025fda2c
Ferdroid8080 <andresf@ferudinato.com>
2019-02-11 10:54:24 -0500

    refactorizado el reducer de autenticacion

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../../helpers/utility';
 
 const initialState = {
     idToken: null,
@@ -9,41 +10,23 @@ const initialState = {
 }
 
 const authReducer = (state = initialState, action) => {
-    if (action.type === actionTypes.AUTH_START) {
-        return {
-            ...state,
-            loading: true
-        }
-    }
+    if (action.type === actionTypes.AUTH_START)
+        return updateObject(state, { loading: true })
     if (action.type === actionTypes.AUTH_SUCCESS) {
-        return {
-            ...state,
+        return updateObject(state, {
             loading: false,
             error: null,
             userId: action.userId,
             idToken: action.idToken
-        }
+        })
     }
-    if (action.type === actionTypes.AUTH_FAIL) {
-        return {
-            ...state,
-            loading: false,
-            error: action.error
-        }
-    }
-    if (action.type === actionTypes.AUTH_LOGOUT) {
-        return {
-            ...state,
-            userId: null,
-            idToken: null
-        }
-    }
-    if (action.type === actionTypes.SET_AUTH_REDIRECT_PATH) {
-        return {
-            ...state,
-            setAuthRedirectPath: action.path
-        }
-    }
+    if (action.type === actionTypes.AUTH_FAIL)
+        return updateObject(state, { loading: false, error: action.error })
+    if (action.type === actionTypes.AUTH_LOGOUT)
+        return updateObject(state, {userId: null, idToken: null}) 
+    if (action.type === actionTypes.SET_AUTH_REDIRECT_PATH)
+        return updateObject(state, { setAuthRedirectPath: action.path })
+
     return state
 }
 
